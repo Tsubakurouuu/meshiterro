b93233fbd2fddfb56b1b5146a17ad4ba2b20af71
Your Name <you@example.com>
2022-08-10 14:28:32 +0000

    [Finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit, :update]
-  
-  get 'homes/top'
-  get "/homes/about" => "homes#about", as: 'about'
   root to: "homes#top"
+  get "/homes/about" => "homes#about", as: 'about'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+    
+    
+  resources :users, only: [:show, :edit, :update]
+  # get 'homes/top'
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
