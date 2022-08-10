fcdb61988d7cd28c353996218450164cbcddf530
Your Name <you@example.com>
2022-08-10 04:45:38 +0000

    [Finish]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit]
-  devise_for :users
+  
   get 'homes/top'
   get "/homes/about" => "homes#about", as: 'about'
   root to: "homes#top"
